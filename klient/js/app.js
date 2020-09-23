71b465dedd6f4fda870ec3db3d5c0bbfc97ea4ba
Alameyqer <67787191+Alameyqer@users.noreply.github.com>
2020-09-23 22:11:35 +0400

    +
    +

---
+++ klient/js/app.js
@@ -0,0 +1,11 @@
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav__list');
+
+	burger.addEventListener('click',()=>{
+
+		nav.classList.toggle('nav__active');
+	});
+}
+
+navSlide();
